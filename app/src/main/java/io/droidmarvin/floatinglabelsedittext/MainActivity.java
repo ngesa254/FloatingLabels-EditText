7b58d7933dd58bc98f08ee1bc5374791bab831da
ngesa marvin <marvinngesa@gmail.com>
2018-02-06 02:56:03 +0300

    update main activity

--- app/src/main/java/io/droidmarvin/floatinglabelsedittext/MainActivity.java
+++ app/src/main/java/io/droidmarvin/floatinglabelsedittext/MainActivity.java
@@ -3,6 +3,7 @@ package io.droidmarvin.floatinglabelsedittext;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
